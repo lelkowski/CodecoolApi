ea0e6d5b62753a98a650007652352765dbbe5b53
Pawel <lelkowski331@gmail.com>
2022-08-09 13:29:21 +0200

    Add for authentication

---
+++ CodecoolApi.Authentication/Context/CodecoolApiIdentityContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodecoolApi.Identity.Context
+{
+    public class CodecoolApiIdentityContext : IdentityDbContext
+    {
+        public CodecoolApiIdentityContext(DbContextOptions<CodecoolApiIdentityContext> options)
+         : base(options)
+        {
+        }
+    }
+}
